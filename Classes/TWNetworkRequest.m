e1c1cec04ad485b401d45935484eeb063d360d24
Christian Menschel <christian@tapwork.de>
2016-10-28 19:07:46 +0200

    Cleans up setting the post parameter

--- Classes/TWNetworkRequest.m
+++ Classes/TWNetworkRequest.m
@@ -10,7 +10,6 @@
 
 @implementation TWNetworkRequest
 {
-    NSString *_postParametersAsString;
     NSMutableDictionary *_HTTPHeaderFields;
 }
 
@@ -46,25 +45,6 @@
 
 #pragma mark - Setter
 
-- (void)setPostParameters:(NSDictionary *)postParameters {
-    if (![_postParameters isEqual:postParameters]) {
-        _postParameters = postParameters;
-        if (!postParameters) {
-            _postParametersAsString = nil;
-        } else {
-            NSMutableString *postString = nil;
-            for (NSString *key in postParameters) {
-                if (!postString) {
-                    [postString appendString:@"&"];
-                }
-                NSString *value = postParameters[key];
-                [postString appendFormat:@"%@=%@", key, value];
-            }
-            _postParametersAsString = postString;
-        }
-    }
-}
-
 - (void)setValue:(nullable NSString *)value forHTTPHeaderField:(NSString *)field
 {
     @synchronized (self) {
@@ -97,16 +77,6 @@
     return authValue;
 }
 
-- (NSTimeInterval)timeout
-{
-    return _timeout;
-}
-
-- (NSURL *)URL
-{
-    return _URL;
-}
-
 - (NSMutableURLRequest *)URLRequest
 {
     NSMutableURLRequest *request = [[NSMutableURLRequest alloc]
@@ -119,8 +89,8 @@
         NSString *value = [NSString stringWithFormat:@"Basic %@", [self HTTPAuth]];
         [request setValue:value forHTTPHeaderField:@"Authorization"];
     }
-    if (self.postParameters && _postParametersAsString) {
-        [request setHTTPBody:[_postParametersAsString dataUsingEncoding:NSUTF8StringEncoding]];
+    if (self.postParameters) {
+        [request setHTTPBody:[self.postParameterAsString dataUsingEncoding:NSUTF8StringEncoding]];
     }
     if (self.HTTPBody) {
         [request setHTTPBody:self.HTTPBody];
@@ -138,6 +108,20 @@
     }
     
     return request;
+}
+
+#pragma mark - Helper
+
+- (NSString *)postParameterAsString {
+    NSMutableString *postString = nil;
+    for (NSString *key in self.postParameters) {
+        if (!postString) {
+            [postString appendString:@"&"];
+        }
+        NSString *value = self.postParameters[key];
+        [postString appendFormat:@"%@=%@", key, value];
+    }
+    return postString;
 }
 
 @end
